63675f6144dd48ebf53c6cec07ab89b847704221
Patrick Sabin <patricksabin@gmx.at>
2011-02-07 12:35:11 +0100

    Added a epdb file to the bin directory, so that the user can start epdb by simply typing 'epdb'

--- make_package.sh
+++ make_package.sh
@@ -1,9 +1,13 @@
 INSTALLDIR=debian/usr/lib/python3.1/dist-packages/
+BINDIR=debian/usr/bin
 DBGMODSDIR=debian/usr/share/epdb/dbgmods
 mkdir -p ${INSTALLDIR}
 cp epdb.py debug.py shareddict.py snapshotting.py dbg.py breakpoint.py resources.py asyncmd.py ${INSTALLDIR}
 python3 -mcompileall ${INSTALLDIR}
 mkdir -p ${DBGMODSDIR}
 cp dbgmods/* ${DBGMODSDIR}
+mkdir -p ${BINDIR}
+cp epdb.py ${BINDIR}/epdb
+chmod 755 ${BINDIR}/epdb
 dpkg-deb --build debian
 mv debian.deb epdb.deb
