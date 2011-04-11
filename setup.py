ab091399167a1ada12a5905f1405ba40468d2943
Patrick Sabin <patricksabin@gmx.at>
2011-04-11 13:15:52 +0200

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(name='epdb',
+      version='0.1',
+      description='Extended Python Debugger',
+      author='Patrick Sabin',
+      author_email='patricksabin@gmx.at',
+      url='http://code.google.com/p/epdb/',
+      packages=['epdblib'],
+      scripts=['bin/epdb']
+      )
